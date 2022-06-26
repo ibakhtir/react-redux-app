ee568b588e872101169c6466333f08a28573fdda
ibakhtir <ibakhtir@gmail.com>
2022-06-26 16:27:46 +0300

    added create task

--- src/services/todos.service.js
+++ src/services/todos.service.js
@@ -1,14 +1,18 @@
 import httpService from "./http.service";
-const todosEndepoint = "todos/";
+const todosEndpoint = "todos/";
 const todosService = {
-    fetch: async () => {
-        const { data } = await httpService.get(todosEndepoint, {
-            params: {
-                _page: 1,
-                _limit: 10,
-            },
-        });
-        return data;
-    },
+  fetch: async () => {
+    const { data } = await httpService.get(todosEndpoint, {
+      params: {
+        _page: 1,
+        _limit: 10,
+      },
+    });
+    return data;
+  },
+  createTask: async (payload) => {
+    const { data } = await httpService.post(todosEndpoint, payload);
+    return data;
+  },
 };
 export default todosService;
